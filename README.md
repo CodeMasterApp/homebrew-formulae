21f193f38a6f6f8a2792745bb744f0ef37c36f2a
N3v1 <n3v1010@gmail.com>
2023-09-16 16:27:47 +0200

    Config

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # homebrew-formulae
+
+> **Note:**\
+> This formula is depending on the CodeMasterCLI repo. Please note that its not done and currently in development
---
+++ codemaster-cli.rb
@@ -0,0 +1,19 @@
+class CodeeditCli < Formula
+    desc "A command-line-interface to open files/folders in CodeMaster.app"
+    homepage "https://github.com/CodeMasterApp/CodeMasterCLI"
+    # url "" # of tar.gz file
+    # sha256 ""
+    license "MIT"
+  
+    depends_on xcode: ["14.0", :build] # update to xcode 15
+    depends_on :macos
+    uses_from_macos "swift"
+  
+    def install
+      system "make", "install", "prefix=#{prefix}"
+    end
+  
+    test do
+      assert_match "OVERVIEW", shell_output("#{bin}/codemaster --help")
+    end
+  end
