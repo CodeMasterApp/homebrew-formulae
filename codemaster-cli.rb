b2123b3ec3c7627705348b44d3013df819ac7ba9
NH <n3v1010@gmail.com>
2023-09-20 19:37:23 +0200

    Update codemaster-cli.rb

--- codemaster-cli.rb
+++ codemaster-cli.rb
@@ -1,19 +1,20 @@
-class CodemasterCli < Formula
-    desc "A command-line-interface to open files/folders in CodeMaster.app"
-    homepage "https://github.com/CodeMasterApp/CodeMasterCLI"
-    # url "" # of tar.gz file
-    # sha256 ""
-    license "MIT"
+class CodeMasterCli < Formula
+  desc "A command-line-interface to open files/folders in CodeMaster.app"
+  homepage "https://github.com/CodeMasterApp/CodeMasterCLI"
+  url "https://github.com/CodeMasterApp/CodeMasterCLI/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "615ff7539eeccbcbba44ca4005d1d64c791c1c87cccad650c8fcf1915c4ef67d"
+  license "MIT"
   
-    depends_on xcode: ["14.0", :build] # update to xcode 15
-    depends_on :macos
-    uses_from_macos "swift"
+  depends_on xcode: ["15.0", :build] # downgrade to "14.0" if needed
+  depends_on :macos
+  uses_from_macos "swift"
   
-    def install
-      system "make", "install", "prefix=#{prefix}"
-    end
-  
-    test do
-      assert_match "OVERVIEW", shell_output("#{bin}/codemaster --help")
-    end
+  def install
+    system "make", "install", "prefix=#{prefix}"
   end
+  
+  test do
+    assert_match "OVERVIEW", shell_output("#{bin}/codemaster --help")
+  end
+end
+
